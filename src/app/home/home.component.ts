0c966c3359dbbcaeb504cab4479961d2224980f3
Marco Herren <marcoherren05@gmx.ch>
2023-05-04 16:22:34 +0200

    did stuff

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { EventService } from '../shared/service/event.service';
 import { Event } from '../shared/interfaces/event';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -10,11 +11,14 @@ import { Event } from '../shared/interfaces/event';
 export class HomeComponent implements OnInit {
   events: Event[] = [];
 
-  constructor(private readonly eventService: EventService) { }
+  constructor(private readonly eventService: EventService, private readonly router: Router) { }
 
   ngOnInit(): void {
     // this.eventService.getEvents().subscribe((data) => {});
     this.events = this.eventService.getEvents();
+  }
+  goToDetails(id: string): void {
+    this.router.navigate([`${id}`]);
   }
 
 }
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,16 +3,19 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { ComponentsModule } from '../components/components.module';
+import { DetailComponent } from './detail/detail.component';
+import { RouterModule } from '@angular/router';
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    DetailComponent
   ],
   imports: [
     CommonModule,
     HomeRoutingModule,
-    ComponentsModule,
+    ComponentsModule
   ]
 })
 export class HomeModule { }
