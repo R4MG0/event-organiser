0c9bca3bde9009f7ed9fd3d5b0f523d531cba08b
Marco Herren <marcoherren05@gmx.ch>
2023-05-04 13:43:10 +0200

    added services and other stuff

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { EventService } from '../shared/service/event.service';
+import { Event } from '../shared/interfaces/event';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  events: Event[] = [];
 
-  constructor() { }
+  constructor(private readonly eventService: EventService) { }
 
   ngOnInit(): void {
+    // this.eventService.getEvents().subscribe((data) => {});
+    this.events = this.eventService.getEvents();
   }
 
 }
